04555ba2126e17afcb18bd3b1ed42b16cf742e51
steve <stevedanko1@gmail.com>
2014-01-23 22:37:09 -0500

    fixed migrations

--- db/migrate/20140124033330_change_time_data_type.rb
+++ db/migrate/20140124033330_change_time_data_type.rb
@@ -4,6 +4,7 @@ class ChangeTimeDataType < ActiveRecord::Migration
     rename_column :sales, :date_sold, :date_sold_string
     add_column :sales, :date_sold, :datetime
   end
+  
   def down 
     rename_column :sales, :date_sold_string, :date_sold
     remove_column :sales, :date_sold
